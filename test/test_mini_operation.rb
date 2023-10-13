da592dca66df7b90ab08f4fc29d46e1fe8f37fae
Andre Durao <andreluisdurao@gmail.com>
2023-10-13 17:57:36 +0900

    Adds a second class for tests

--- test/test_mini_operation.rb
+++ test/test_mini_operation.rb
@@ -2,12 +2,20 @@
 
 require "test_helper"
 
-class Foo
+class FirstOp
   include MiniOperation
 
   step :load
   step :calculate
   step :save
+end
+
+class SecondOp
+  include MiniOperation
+
+  step :collect
+  step :format
+  step :submit
 end
 
 class TestMiniOperation < Minitest::Test
@@ -16,7 +24,10 @@ class TestMiniOperation < Minitest::Test
   end
 
   def test_it_should_initialize_a_class_variable
-    steps = Foo.class_variable_get(:@@__mini_operation_steps)
-    assert_equal([:load, :calculate, :save], steps)
+    steps = FirstOp.class_variable_get(:@@__mini_operation_steps)
+    assert_equal(%i[load calculate save], steps)
+
+    steps = SecondOp.class_variable_get(:@@__mini_operation_steps)
+    assert_equal(%i[collect format submit], steps)
   end
 end
